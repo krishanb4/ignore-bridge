411f9913be28f9892a832113dcaab34db4775a5d
Dev Cresix <52518140+devcresix@users.noreply.github.com>
2023-04-30 14:02:50 +0530

    address update

--- src/config/constants/addresses.tsx
+++ src/config/constants/addresses.tsx
@@ -1,6 +1,6 @@
 //for 4TOKEN
-export const bscContractAddress = "0xfec987B2E552cBf35656a74f30A6de0D766FBd88";
-export const coreContractAddress = "0xfec987B2E552cBf35656a74f30A6de0D766FBd88";
+export const bscContractAddress = "0x86046FF6f60A87CEA907CC60A1A762E9836fF42e";
+export const coreContractAddress = "0x186f97250e320731A87240CA62cDa8406f58CA1b";
 
 //for USDT
 //export const bscContractAddress = "0xf953f9FfA5c1f9F55fD8408C24D23850F1a35213";
@@ -23,7 +23,7 @@ export const tokens: Tokens = {
     core: "0x148a24e5c355455268B4d04823770280dD023267",
   },
   IGNORE: {
-    bsc: "0x57a764ddad32b816Bd8b70E8d9a1391F7642347d",
+    bsc: "0xacD29Bf81c73a675a4ae33c23Ee143E89bD2aB8f",
     core: "0x98564E70c7fCC6d947fFE6d9EfeD5ba68b306F2E",
   },
 };
