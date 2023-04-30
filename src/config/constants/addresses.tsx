d421535e56bef8a9a315ff78ef7f631e81928bfe
Dev Cresix <52518140+devcresix@users.noreply.github.com>
2023-04-30 19:46:28 +0530

    address update after burn error

--- src/config/constants/addresses.tsx
+++ src/config/constants/addresses.tsx
@@ -1,6 +1,6 @@
 //for 4TOKEN
-export const bscContractAddress = "0x86046FF6f60A87CEA907CC60A1A762E9836fF42e";
-export const coreContractAddress = "0x186f97250e320731A87240CA62cDa8406f58CA1b";
+export const bscContractAddress = "0x04cbefb3D208172336F74448d529148b429d4D7F";
+export const coreContractAddress = "0x8e4E0d8Db2115b5A6bd1062D32f51A0194088442";
 
 //for USDT
 //export const bscContractAddress = "0xf953f9FfA5c1f9F55fD8408C24D23850F1a35213";
@@ -23,7 +23,7 @@ export const tokens: Tokens = {
     core: "0x148a24e5c355455268B4d04823770280dD023267",
   },
   IGNORE: {
-    bsc: "0xacD29Bf81c73a675a4ae33c23Ee143E89bD2aB8f",
+    bsc: "0xb1a147afc3Ec72F65340b53F9D3F4BB2d0D46462",
     core: "0x98564E70c7fCC6d947fFE6d9EfeD5ba68b306F2E",
   },
 };
